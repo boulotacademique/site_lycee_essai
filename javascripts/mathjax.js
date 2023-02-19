6e4c14625a81d2c660c2f67fcbffb2d34fc20039
frodonsfr <frodonsfr@gmail.com>
2023-02-19 12:05:21 +0100

    Deployed 6e70ff6 with MkDocs version: 1.1.2

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -1,14 +1,31 @@
 window.MathJax = {
    tex: {
      inlineMath: [["\\(", "\\)"]],
-     displayMath: [["\\[", "\\]"]],
+     displayMath: [["$$", "$$"],["\\[", "\\]"]],
      processEscapes: true,
      processEnvironments: true,
      macros:{
       N : '{\\mathbb{N}}',
       R : '{\\mathbb{R}}',
+      lp : '{\\left(}',
+      rp : '{\\right)}',
+      ex : '{\\text{e}}',
+      equivaut : '{\\iff}',
       Syst : ['\\left\\{ \\begin{array}{rcl} #1\\\\ #2 \\end{array} \\right.', 2],
-      cad : "c'est-à-dire"
+      dlim : ['\\displaystyle{\\lim_{#1 \\to #2}}',2],
+      Pb : '{\\mathbb{P}}',
+      comb : ['\\displaystyle{\\left(\\begin{array}{c} #1 \\\\ #2 \\end{array}\\right)}',2],
+      dvec : ['\\overrightarrow{#1}',1],
+      vect : ['\\overrightarrow{#1}',1],
+      vec : ['\\overrightarrow{#1}',1],
+      V : ['\\overrightarrow{#1}',1],
+      mc : ['\\mathcal{#1}',1],
+      coordVecEsp: ['\\left(\\begin{array}{c} #1 \\\\ #2 \\\\ #3\\end{array}\\right)',3],
+      vectCoEsp : ['\\vect{#1}=\\coordVecEsp{#2}{#3}{#4}',4],
+      Lim: ['\\displaystyle{\\left. \\begin{array}{lcl} #1 \\\\ #2 \\\\ \\end{array} \\right\\} }',2],
+      Oij: '{\\left( O;\\vec{i},\\vec{j}\\right)}',
+      Oijk: '{\\left( O;\\vec{i},\\vec{j},\\vec{k}\\right)}',
+        
      }
    },
    options: {
